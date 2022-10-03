b35d909a0dccaeff34e71e4e09ed7118c3bd3f87
Vidi Server <74022888+vicserv@users.noreply.github.com>
2022-10-03 09:56:11 -0500

    remplace promesa

--- app.js
+++ app.js
@@ -11,39 +11,36 @@ const main = async (uid) => {
     try {
 
         // The url of the radio
-        const url = `https://feed.tunein.com/profiles/s${uid}/nowPlaying?itemToken=BgUFAAEAAQABAAEAb28BgmABAAEFAAA&formats=mp3,aac,ogg,flash,html,hls&serial=8c31f318-8c19-41cd-b8db-7ae18538e302&partnerId=RadioTime&version=5.2602&itemUrlScheme=secure&reqAttempt=1`;
+        const url = `https://feed.tunein.com/profiles/s${uid}/nowPlaying?token=eyJwIjpmYWxzZSwidCI6IjIwMjItMTAtMDNUMTM6MjM6MTYuNjk2Mjg2MloifQ&itemToken=BgUFAAEAAQABAAEAb28BEWAAAAEFAAA&formats=mp3,aac,ogg,flash,html,hls&serial=8c31f318-8c19-41cd-b8db-7ae18538e302&partnerId=RadioTime&version=5.2602&itemUrlScheme=secure&reqAttempt=1`;
 
-        const response = await axios.get(url).then(response => response.data).catch(error => console.log('no' + uid));
-        const { Primary } = response;
-        const { Image, GuideId } = Primary;
+        const data_radio = await axios.get(url).then(response => response.data).catch(error => console.log('no' + uid));
+        if (data_radio) {
+            radios.push(data_radio);
+            fs.writeFileSync('radios.json', JSON.stringify(radios));
+        }
+
+
+
+        // The url of the m3u file
+        const url_m3u = `https://opml.radiotime.com/Tune.ashx?id=s${uid}`;
+
+        //Get m3u file
+        await axios.get(url_m3u).then(response => response.data).then(data => {
+            if (data.includes('#STATUS')) {
+                console.log('error ', uid)
+            } else {
+                fs.writeFileSync(`./m3u/${uid}.m3u`, data);
+            }
+        })
+
+
+
+        // Verify result of Promesa
+
+        const { Image } = data_radio.Primary;
 
         // Extension of Image
         const extension = Image.split('.')[Image.split('.').length - 1].split('?')[0];
-        // The url of the m3u file
-        const url_m3u = `https://opml.radiotime.com/Tune.ashx?id=${GuideId}`;
-
-        //Get m3u file
-        const m3uResponse = await axios.get(url_m3u).then(response => response.data)
-
-        //Verify Status of m3u file
-        const promesa = new Promise((resolve, reject) => {
-            m3uResponse.on('data', chunk => {
-                if (chunk.includes('#STATUS')) {
-                    reject('error');
-                } else {
-                    fs.writeFileSync(`./m3u/${uid}.m3u`, chunk);
-                    resolve('success');
-                }
-            });
-        });
-
-        // Save result of Promesa
-        await promesa;
-
-        // Verify result of Promesa
-        radios.push(response);
-        fs.writeFileSync('radios.json', JSON.stringify(radios));
-
         // Download the image
         await axios.get(Image, { responseType: 'stream' }).then(response => {
             response.data.pipe(fs.createWriteStream(`./images/${uid}.${extension}`));
